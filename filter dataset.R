03977296309d4def89bdf76da2c3b9bbf65c5518
Jesus <rpsoft@gmail.com>
2021-02-19 20:52:35 +0000

    shorter dataset

--- filter dataset.R
+++ filter dataset.R
@@ -8,11 +8,15 @@ library(tidyverse)
 # training$target %>% unique
 # 
 
-
-
-goodreads_books %>% select( id, average_rating, title, author, description, genre_and_votes ) %>% 
+books_dataset <- goodreads_books %>% select( id, average_rating, title, author, description, genre_and_votes ) %>% 
   filter( ! description %>% is.na() ) %>% filter( ! genre_and_votes %>% is.na() ) %>% 
   mutate( genre = gsub('[[:digit:]]+', '', genre_and_votes)  ) %>% 
-  mutate( genre = sapply( strsplit(genre, ','), `[`, 1 )) %>% select(-genre_and_votes) %>% View
+  mutate( genre = sapply( strsplit(genre, ','), `[`, 1 )) %>% select(-genre_and_votes,-id) 
 
+books_dataset <- books_dataset %>% head(35000)
 
+books_dataset <- books_dataset %>% filter( genre %in% c("Fiction ","Fantasy ","Romance ","Young Adult ","Nonfiction ","Historical-Historical Fiction ","Mystery ","Science Fiction ","History "))
+
+books_dataset %>% write_csv("books_dataset.csv")
+ 
+table(books_dataset$genre) %>% View
